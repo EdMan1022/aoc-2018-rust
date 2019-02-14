10dc3a88fcf4b74ba8a6c9b733294ce14f526c9e
Edward Brennan <EdMan1022@gmail.com>
2019-02-14 17:59:25 -0500

    Update main function to correctly parse input data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
+
+mod argument_parser;
+mod file_parser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args[1]);
+ 
+    let filename = argument_parser::parse_config(&args);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let data = file_parser::parse(&contents);
+
+    println!("With text:\n{:?}", data);
 }
