ff4b60e33826f98bbaa31f462baa8ae10ce41784
Edward Brennan <EdMan1022@gmail.com>
2019-02-14 17:34:47 -0500

    Add test and interface for parse function

---
+++ src/file_parser.rs
@@ -0,0 +1,19 @@
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+
+        let input_data = "+1\n-2\n+3".to_string();
+        let output_data = vec!(1, -2, 3);
+
+        assert_eq!(parse(&input_data), output_data);
+    }
+}
+
+
+pub fn parse(input: &str) -> Vec<i32> {
+    
+}
