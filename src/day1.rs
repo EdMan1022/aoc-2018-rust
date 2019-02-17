74b278e53c2e5a52590856503789808c1e04c88b
Edward Brennan <EdMan1022@gmail.com>
2019-02-17 14:16:53 -0500

    Add a `struct` for representing program state for part 2 exercize

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -22,25 +24,25 @@ mod test {
     fn test_part2_1() {
         let input = vec!(1, -1);
         let output = 0;
-        assert_eq!(part2(&input), output)
+        assert_eq!(part2(input), output)
     }
     #[test]
     fn test_part2_2() {
         let input = vec!(3, 3, 4, -2, -4);
         let output = 10;
-        assert_eq!(part2(&input), output)
+        assert_eq!(part2(input), output)
     }
     #[test]
     fn test_part2_3() {
         let input = vec!(-6, 3, 8, 5, -6);
         let output = 5;
-        assert_eq!(part2(&input), output)
+        assert_eq!(part2(input), output)
     }
     #[test]
     fn test_part2_4() {
         let input = vec!(7, 7, -2, -7, -4);
         let output = 14;
-        assert_eq!(part2(&input), output)
+        assert_eq!(part2(input), output)
     }
 }
 
@@ -65,6 +67,22 @@ pub fn part1(data: &Vec<i32>) -> i32 {
         sum += i;
     }
     sum
+}
+
+struct Frequencies {
+    changes: Vec<i32>,
+    updated: HashSet<i32>,
+    current: i32
+}
+
+impl Frequencies {
+    fn new(changes: Vec<i32>, updated: HashSet<i32>, current: i32) -> Frequencies {
+        Frequencies{ 
+            changes: changes,
+            updated: updated,
+            current: current
+        }
+    }
 }
 
 /// Finds the first repeat sum of adajecent elements in a vector
