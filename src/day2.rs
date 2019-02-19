f42eca85722c5bc7634463b6fc1b811545f35685
Edward Brennan <EdMan1022@gmail.com>
2019-02-18 21:13:13 -0500

    initial implementation of part1

--- src/day2.rs
+++ src/day2.rs
@@ -3,55 +3,39 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_part_1_1() {
-        let input = "abcdef";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
+    fn test_part_1() {
+        let input = vec!("abcdef".to_string(), "bababc".to_string(),
+        "abbcde".to_string(), "abcccd".to_string(), "aabcdd".to_string(),
+        "abcdee".to_string(), "ababab".to_string());
+        let output = 12;
+        assert_eq!(part_1(&input), output)
 
-    }
-
-    #[test]
-    fn test_part_1_2() {
-        let input = "bababc";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
-
-    }
-    #[test]
-    fn test_part_1_3() {
-        let input = "abbcde";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
-    }
-    #[test]
-    fn test_part_1_4() {
-        let input = "abcccd";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
-    }
-    #[test]
-    fn test_part_1_5() {
-        let input = "aabcdd";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
-    }
-
-    #[test]
-    fn test_part_1_6() {
-        let input = "abcdee";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)
-        
-    }
-
-    #[test]
-    fn test_part_1_7() {
-        let input = "ababab";
-        let output = "";
-        assert_eq!(part_1(input.to_string()), output)        
     }
 }
 
-pub fn part_1(input: String) -> String {
-    "output".to_string()
+
+pub fn part_1(input: &Vec<String>) -> i32 {
+
+    let mut two_count = 0;
+    let mut three_count = 0;
+
+    for item in input {
+
+        let mut two_match = false;
+        let mut three_match = false;
+
+        for character in item.chars() {
+            let match_count = item.matches(character).count();
+            if match_count == 2 {two_match = true;}
+            if match_count == 3 {three_match = true;}
+        }
+        if two_match {
+            two_count += 1;
+        }
+        if three_match {
+            three_count += 1;
+        }
+    }
+
+    two_count * three_count
 }
